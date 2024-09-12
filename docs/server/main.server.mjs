8b9622f80397ff476d842a1967ae616bc126ac14
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2024-09-12 11:26:23 +0300

    Build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LX4AGA7Z.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-IRLIE5XL.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
