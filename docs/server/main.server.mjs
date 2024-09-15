43eb2b72f3240406cc130c1d26c673992524573c
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2024-09-15 19:56:43 +0300

    Update

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7Y2XLXCK.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-Q5QN2NAS.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
