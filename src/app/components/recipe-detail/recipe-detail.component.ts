cde5f5dc7502724f29f4fd76debfae4ffe85aaa1
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2024-09-17 02:24:46 +0300

    Update

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: any;
   ingredients: string[] = [];
   errorMessage: string = '';
+  loading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,16 +25,24 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      this.recipeService.getRecipeDetails(id).subscribe((data) => {
-        if (data && data.meals) {
-          this.recipe = data.meals[0];
-          this.ingredients = this.getIngredients();
-        } else {
-          this.errorMessage = 'No recipe data found.';
+      this.recipeService.getRecipeDetails(id).subscribe(
+        (data) => {
+          if (data && data.meals) {
+            this.recipe = data.meals[0];
+            this.ingredients = this.getIngredients();
+          } else {
+            this.errorMessage = 'No recipe data found.';
+          }
+          this.loading = false;
+        },
+        (error) => {
+          this.errorMessage = 'An error occurred while fetching recipe data.';
+          this.loading = false;
         }
-      });
+      );
     } else {
       this.errorMessage = 'No recipe ID provided in the route.';
+      this.loading = false;
     }
   }
 
