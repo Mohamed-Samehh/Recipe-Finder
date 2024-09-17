ca2afd3b6d2ab81b1b611efaa4cbd3cfe65c23af
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2024-09-17 11:18:33 +0300

    Update

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GEXRLJ5H.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OJFB2NGD.mjs";import"./chunk-VOG3WYSX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
